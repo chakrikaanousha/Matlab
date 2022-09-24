e6b5150cd0519e806db31ac1792c65197db798f1
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-09-24 10:08:44 +0530

    Update Solve a Linear Equation.m

--- SolvingNon-Homogenous/Solve a Linear Equation.m
+++ SolvingNon-Homogenous/Solve a Linear Equation.m
@@ -1,1 +1,15 @@
 
+%% SOLVING NONHOMOGENOUS EQUATION:
+%% Write a MATLAB code for a Non-  system of two linear equation having a unique solution: 3x + 2y = 1 and 2x + 3y = 2
+
+%% code:
+clc
+clear
+%specifying variables
+syms x y z
+eqn1=3*x+2*y==1;
+eqn2=2*x+3*y==2;
+%applying function
+sol = solve([eqn1,eqn2],[x,y,z],'ReturnCondition',true)
+disp('solution is:')
+[sol.x;sol.y;sol.z]
