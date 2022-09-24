5e429e0904f7a239f4e5b3d5b4d6a2035813abf6
Chakrika_Anousha_ <101462904+chakrikaanousha@users.noreply.github.com>
2022-09-24 09:56:00 +0530

    Update SolvingNon-Homogenous.m

--- SolvingNon-Homogenous.m
+++ SolvingNon-Homogenous.m
@@ -1,7 +1,7 @@
-/* SOLVING NONHOMOGENOUS EQUATION:
-Write a MATLAB code for a Non-  system of two linear equation having a unique solution:
-3x + 2y = 1 and 2x + 3y = 2 */
-%%
+%% SOLVING NONHOMOGENOUS EQUATION:
+%% Write a MATLAB code for a Non-  system of two linear equation having a unique solution: 3x + 2y = 1 and 2x + 3y = 2
+
+%% code:
 clc
 clear
 %specifying variables
